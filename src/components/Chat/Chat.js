4d9934c5c655242e13af68ba871f9a1a1a4342d0
Andres Icedo <andresicedo24@gmail.com>
2021-04-14 13:37:15 -0500

    updated translation js

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -6,6 +6,7 @@ import Messages from '../Messages/Messages';
 import InfoBar from '../InfoBar/InfoBar';
 import Input from '../Input/Input';
 import './Chat.css';
+import Translation from '../Translation/Translation';
 
 const ENDPOINT = 'https://wiser-chat.herokuapp.com/';
 let socket;
@@ -57,6 +58,7 @@ const Chat = ({ location }) => {
           <Messages messages={messages} name={name} />
           <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
       </div>
+      <Translation message={message}/>
       <TextContainer users={users}/>
     </div>
   );
--- src/components/Translation/Translation.js
+++ src/components/Translation/Translation.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react'
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 
 
-export default function Translation() {
+export default function Translation({ message }) {
     const [ translation, setTranslation ] = useState('');
-    const message = useSelector((state) => {
-        return state.message;
-    })
+    // const message = useSelector((state) => {
+    //     return state.message;
+    // })
 
     useEffect(() => {
         fetch(`https://api.funtranslations.com/translate/yoda.json?text=${message}`)
