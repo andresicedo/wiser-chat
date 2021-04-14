5b247a1564c58d03295b856cdad8c491f8d8689e
Andres Icedo <andresicedo24@gmail.com>
2021-04-14 09:27:48 -0500

    updated file chat file

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,16 +1,13 @@
 import React, { useState, useEffect } from "react";
 import queryString from 'query-string';
 import io from "socket.io-client";
-
 import TextContainer from '../TextContainer/TextContainer';
 import Messages from '../Messages/Messages';
 import InfoBar from '../InfoBar/InfoBar';
 import Input from '../Input/Input';
-
 import './Chat.css';
 
 const ENDPOINT = 'https://wiser-chat.herokuapp.com/';
-
 let socket;
 
 const Chat = ({ location }) => {
