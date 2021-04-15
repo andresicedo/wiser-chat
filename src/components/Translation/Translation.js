265e747e79eaad48d0260a9848b62a464faca6c8
Andres Icedo <andresicedo24@gmail.com>
2021-04-15 11:37:18 -0500

    updated api

--- src/components/Translation/Translation.js
+++ src/components/Translation/Translation.js
@@ -1,17 +1,13 @@
 import React, { useEffect, useState } from 'react'
-// import { useSelector } from 'react-redux';
 const translate = require('google-translate-api');
 
 export default function Translation({ message }) {
     const [translation, setTranslation] = useState('');
-    // const message = useSelector((state) => {
-    //     return state.message;
-    // })
+
     useEffect(() => {
-        translate(message, { to: 'en' })
+        translate(message, { to: 'es' })
         .then(res => {
             setTranslation(res.text);
-            //=> I speak English
         }).catch(err => {
             alert(err);
         });
