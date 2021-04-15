b523029cf1bdc09a488c836828aabcb142c0bbbd
Andres Icedo <andresicedo24@gmail.com>
2021-04-15 11:18:04 -0500

    updated api

--- src/components/Translation/Translation.js
+++ src/components/Translation/Translation.js
@@ -1,26 +1,18 @@
 import React, { useEffect, useState } from 'react'
 // import { useSelector } from 'react-redux';
-
+const translate = require('google-translate-api');
 
 export default function Translation({ message }) {
-    const [translation, setTranslation] = useState('');
+    const [ translation, setTranslation ] = useState('');
     // const message = useSelector((state) => {
     //     return state.message;
     // })
 
     useEffect(() => {
-        fetch("https://systran-systran-platform-for-language-processing-v1.p.rapidapi.com/translation/text/translate?source=auto&target=Spanish&input=Hello", {
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-key": "30d05dba14mshcbd9ba2ffdd2fc7p1f0307jsn8b31de4705a3",
-                "x-rapidapi-host": "systran-systran-platform-for-language-processing-v1.p.rapidapi.com"
-            }
-        })
-            .then(response => {
-                setTranslation(response);
-            })
-            .catch(err => {
-                console.error(err);
+        translate(message, {to: 'es'})
+            .then((res) => res.json())
+            .then((data) => {
+                setTranslation(data.text);
             });
     }, [message]);
 
