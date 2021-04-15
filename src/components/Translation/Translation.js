03f1a5b0d3cd5182fc6dfa8f2cd8d6eaaf7c0288
Andres Icedo <andresicedo24@gmail.com>
2021-04-15 10:25:16 -0500

    updated api

--- src/components/Translation/Translation.js
+++ src/components/Translation/Translation.js
@@ -9,16 +9,23 @@ export default function Translation({ message }) {
     // })
 
     useEffect(() => {
-        fetch(`https://api.funtranslations.com/translate/yoda.json?text=${message}`)
+        fetch(`https://systran-systran-platform-for-language-processing-v1.p.rapidapi.com/translation/text/translate?source=English&target=Spanish&input=${message}`, {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-key": "30d05dba14mshcbd9ba2ffdd2fc7p1f0307jsn8b31de4705a3",
+                "x-rapidapi-host": "systran-systran-platform-for-language-processing-v1.p.rapidapi.com"
+            }
+        })
             .then((res) => res.json())
             .then((data) => {
-                setTranslation(data.contents);
+                console.log(data)
+                setTranslation(data);
             });
     }, [message]);
 
     return (
         <div>
-            {translation.translated}
+            {translation}
         </div>
     )
 }
--- src/components/Translation/osv.js
+++ src/components/Translation/osv.js
@@ -1,42 +1,43 @@
-// let WordPOS = require('wordpos'),
-// wordpos = new WordPOS();
+let WordPOS = require('wordpos'),
+wordpos = new WordPOS();
 
-// let objects = [];
-// let subject = [];
-// let verb = [];
-// let result = [];
+let objects = [];
+let subject = [];
+let verb = [];
+let result = [];
 
 // function osv(string) {
 //     let words = string.split(" ");
 //     let osvObject = wordpos.getPOS(string);
 //     let keys = Object.keys(osvObject);
-    // for (let i = 0; i < words.length; i++) {
-    //     const word = words[i];
-    //     const nextWord = words[i+1];
-    //     if(word === 'a' || 'A') {
-    //         objects.push(word, nextWord);
-    //     }
-    //     if(word === 'the' || 'The') {
-    //         subject.push(word, nextWord)
-    //     }
-    //     if(osvObject[keys[0]].includes(word) && !objects.includes(word)) {
-    //         objects.push(word)
-    //     }
-    //     if(osvObject[keys[1]].includes(word) && !verb.includes(word)) {
-    //         verb.push(word)
-    //     }
-    //     if(osvObject[keys[2]].includes(word) && !objects.includes(word)) {
-    //         objects.push(word)
-    //     }
-    //     if(osvObject[keys[2]].includes(word) && !subject.includes(word)) {
-    //         subject.push(word)
-    //     }
+//     for (let i = 0; i < words.length; i++) {
+//         const word = words[i];
+//         const nextWord = words[i+1];
+//         if(word === 'a' || 'A') {
+//             objects.push(word, nextWord);
+//         }
+//         if(word === 'the' || 'The') {
+//             subject.push(word, nextWord)
+//         }
+//         if(osvObject[keys[0]].includes(word) && !objects.includes(word)) {
+//             objects.push(word)
+//         }
+//         if(osvObject[keys[1]].includes(word) && !verb.includes(word)) {
+//             verb.push(word)
+//         }
+//         if(osvObject[keys[2]].includes(word) && !objects.includes(word)) {
+//             objects.push(word)
+//         }
+//         if(osvObject[keys[2]].includes(word) && !subject.includes(word)) {
+//             subject.push(word)
+//         }
         
-    // }
-    // let osvString = result.push(objects, subject, verb).toString();
-//     return osvObject;
+//     }
+//     let osvString = result.push(objects, subject, verb).toString();
+//     return osvString;
 // }
 
+wordpos.getPOS('The woman built a strong stone wall', console.log)
 
 // console.log(osv('The woman built a strong stone wall'))
 // //The woman [S] built [V] a strong stone wall [O]
