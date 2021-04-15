4b8f2b310b97dd0c73b40569f22f51f91e9cd43b
Andres Icedo <andresicedo24@gmail.com>
2021-04-15 10:41:10 -0500

    updated api

--- src/components/Translation/Translation.js
+++ src/components/Translation/Translation.js
@@ -3,23 +3,24 @@ import React, { useEffect, useState } from 'react'
 
 
 export default function Translation({ message }) {
-    const [ translation, setTranslation ] = useState('');
+    const [translation, setTranslation] = useState('');
     // const message = useSelector((state) => {
     //     return state.message;
     // })
 
     useEffect(() => {
-        fetch(`https://systran-systran-platform-for-language-processing-v1.p.rapidapi.com/translation/text/translate?source=English&target=Spanish&input=${message}`, {
+        fetch("https://systran-systran-platform-for-language-processing-v1.p.rapidapi.com/translation/text/translate?source=%7Bsource%7D&target=%7Btarget%7D&input=%7Binput%7D", {
             "method": "GET",
             "headers": {
                 "x-rapidapi-key": "30d05dba14mshcbd9ba2ffdd2fc7p1f0307jsn8b31de4705a3",
                 "x-rapidapi-host": "systran-systran-platform-for-language-processing-v1.p.rapidapi.com"
             }
         })
-            .then((res) => res.json())
-            .then((data) => {
-                console.log(data)
-                setTranslation(data);
+            .then(response => {
+                console.log(response);
+            })
+            .catch(err => {
+                console.error(err);
             });
     }, [message]);
 
