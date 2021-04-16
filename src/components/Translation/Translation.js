f30d3ddbdb4f666efe716a933a7602b603906d57
Andres Icedo <andresicedo24@gmail.com>
2021-04-16 13:20:59 -0500

    updated translationjs

--- src/components/Translation/Translation.js
+++ src/components/Translation/Translation.js
@@ -1,13 +1,12 @@
 import React from 'react'
 
-export default function Translation({ message: { user, text, translated }}) {
+export default function Translation({ message: { user, text, translation }}) {
     return (
         <div>
             <div>
                 <h4>Recent message by: {user}</h4>
-                {console.log(translated)}
-                <p>{translated}</p>
-                <h5><i>({text})</i></h5>
+                <p>{translation}</p>
+                <h5>(<i>{text}</i>)</h5>
             </div>
         </div>
     )
