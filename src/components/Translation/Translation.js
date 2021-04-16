82d3fb753de475eae0a7ae02363be8eb82740584
Andres Icedo <andresicedo24@gmail.com>
2021-04-16 14:22:41 -0500

    updated translation js

--- src/components/Translation/Translation.js
+++ src/components/Translation/Translation.js
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import moment from 'moment';
 export default function Translation({ message: { user, text, translation }}) {
     return (
         <div>
             <div>
-                <h4>Recent message by: {user}</h4>
+                <h4>Most recent message: {user} about {moment().startOf('hour').fromNow()}</h4>
                 <p>{translation}</p>
                 <h5>(<i>{text}</i>)</h5>
             </div>
